7a1e3ccbc8c4facda9be371b5b0e456e1723ebed
CameronScully <camscully@hotmail.com>
2020-01-03 11:10:11 +1100

    First commit

---
+++ src/index.js
@@ -0,0 +1,161 @@
+//react libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+//bootstrap stylesheet
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//awesome font icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+import { faThumbsDown } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+
+class ThumbsUp extends React.Component{
+  render(){
+    return(
+      <FontAwesomeIcon icon={faThumbsUp} />
+    );
+  }
+}
+
+class ThumbsDown extends React.Component{
+  render(){
+    return(
+      <FontAwesomeIcon icon={faThumbsDown} />
+    );
+  }
+}
+
+class Trash extends React.Component {
+  render(){
+    return(
+      <FontAwesomeIcon icon={faTrashAlt} />
+    );
+  }
+}
+
+class Dare extends React.Component{
+  render(){
+    return(
+      <div class="container" id="dareContainer">
+        <div class="row align-items-center">
+          <div class="col" align="center">
+            <Trash />
+          </div>
+          <div class="col" align="center" id="dareText">
+            dare text placeholder/4
+          </div>
+          <div class="col" align="center">
+            <div class="container">
+              <div class="row">
+                <div class="col">
+                  <ThumbsUp />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <ThumbsDown />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Slider extends React.Component {
+  render(){
+    return(
+      <input type="range" min="0" max="5"/>
+    );
+  }
+}
+
+class Player extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {value: "Player name"};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+
+  render(){
+    return(
+      <div class="container shadow" id="player">
+        <div class="row">
+          <div class="col" align="center">
+            <form>
+              <input type="text" id="username" value={this.state.value} onChange={this.handleChange}/>
+            </form>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col" align="center">
+            <Slider />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <Dare />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+//class
+class AddPlayer extends React.Component {
+  render(){
+    return(
+      <div>
+        <button>
+          <FontAwesomeIcon icon={faPlusCircle} size="2x" />
+        </button>
+      </div>
+    );
+  }
+}
+
+class DareList extends React.Component {
+  render() {
+    return(
+      <div class="container-fluid" align="center">
+        <div class="row" id="banner">
+          <div class="col">
+            insert dropdown here
+          </div>
+          <div class="col">
+            <h1>darelist</h1>
+          </div>
+          <div class="col"></div>
+        </div>
+        <div class="row">
+          <div class="col-1 offset-md-2">
+            <AddPlayer />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <Player />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <DareList />,
+  document.getElementById('root')
+);

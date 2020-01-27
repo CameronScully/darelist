0c52103f2c6418b3e03467f80c4022f6731a7ead
CameronScully <camscully@hotmail.com>
2020-01-27 13:24:10 +1100

    includes build folder

---
+++ client/build/precache-manifest.b2ec81102bad1a2613fd7c59ba1a3ba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee569e81c48c4a2f6fa48fdbddf2784",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5ef04f4fe493ac02fce",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ca299588b43312ab167a",
+    "url": "/static/css/main.76608351.chunk.css"
+  },
+  {
+    "revision": "d5ef04f4fe493ac02fce",
+    "url": "/static/js/2.7777e6c1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.7777e6c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca299588b43312ab167a",
+    "url": "/static/js/main.ef3e00a0.chunk.js"
+  },
+  {
+    "revision": "81ff23e32dbef0087176",
+    "url": "/static/js/runtime-main.76aba886.js"
+  }
+]);
